c462eaf643ffa041a0a3743f5f5b0915f71b5dc1
Jakub Mareda <mareda@techsys.cz>
2017-04-26 18:16:52 +0200

    Zaplneny krouzek

--- ShapeRendering.h
+++ ShapeRendering.h
@@ -7,4 +7,5 @@ void setPixel(std::vector<Color>& pixmap, const Color& color, int x, int y, int 
 void line(std::vector<Color>& pixmap, const Color& color, int x1, int y1, int x2, int y2, int width);
 void lineAngle(std::vector<Color>& pixmap, const Color& color, int x1, int y1, double angle, int length, int width);
 void circle(std::vector<Color>& pixmap, const Color& color, int r, int x0, int y0, int width);
+void circleFill(std::vector<Color>& pixmap, const Color& color, int r, int x0, int y0, int width);
 #endif // SHAPERENDERING_H
