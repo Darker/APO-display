7f9b522884e7a72115056ff56bd6933a6730ac83
Minimumtabaku <ponomrenko.yevgen@yahoo.com>
2017-04-26 17:40:14 +0200

    shape jsem asi delal

--- Shape.cpp
+++ Shape.cpp
@@ -1,6 +1,15 @@
 #include "Shape.h"
+#include <string>
 
-Shape::Shape()
-{
 
-}
+
+
+ Shape::Shape(std::string name,Color color,int height, int width)
+     :name(name)
+     ,color(color)
+     ,height(height)
+     ,width(width){}
+
+
+
+
--- Shape.h
+++ Shape.h
@@ -2,12 +2,20 @@
 #define SHAPE_H
 
 #include "Color.h"
+#include <string>
 class Shape
 {
 public:
     Shape();
     virtual void render(std::vector<Color>& pixmap, int width, int height) const = 0;
     virtual Shape* cloneNew() const = 0;
+    Shape(std::string name,Color color, double height, double length);
+    //properties
+    std::string name;
+    Color color;
+    double height;
+    double width;
+
 };
 
 #endif // SHAPE_H
