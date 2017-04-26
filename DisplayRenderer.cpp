5810f936c944f31fdad760508545ca3da94916d4
Jakub Mareda <mareda@techsys.cz>
2017-04-26 17:44:59 +0200

    moar commit

--- DisplayRenderer.cpp
+++ DisplayRenderer.cpp
@@ -86,8 +86,7 @@ void DisplayRenderer::updatePixmapCache()
     if(pixmapChanged) {
         pixmapCache.resize(pixmap.size());
         for(size_t i=0, l=pixmap.size(); i+1<l; i+=2) {
-            uint32_t colors = pixmap[i];
-            colors<<16;
+            uint32_t colors = ((uint32_t)pixmap[i])<<16;
             colors = colors | pixmap[i+1];
 
             pixmapCache[i] = colors;
